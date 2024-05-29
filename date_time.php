262f4cc1b961b35e61936da3b0c90cd14d65dcfc
shafrinahamed97 <shafrinahamedjob@gmail.com>
2024-05-29 17:57:52 +0600

    Finished date time php

--- date_time.php
+++ date_time.php
@@ -1,7 +1,19 @@
 <?php
 $date = strtotime('today');
 
-echo date('Y-m-d H:i:s', $date);
+// echo date('d-m-Y H:i:s', $date);
 
 $dateafter10days = strtotime('+10 days');
-echo date('Y-m-d H:i:s', $dateafter10days);
+// echo date('d-m-Y H:i:s', $dateafter10days);
+
+// number of week in current year
+$week = date('W');
+echo $week ."\n";
+
+// tomorrow date
+$tomorrow = strtotime('tomorrow');
+echo date('d-m-Y H:i:s', $tomorrow) ."\n";
+
+$date = strtotime('22nd June 2024');
+echo date('d-m-Y', $date);
+
