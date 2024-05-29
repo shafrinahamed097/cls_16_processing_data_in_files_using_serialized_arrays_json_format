f71ed1d6f0a0b5d2cdf8eec806cab5902bccdfe3
shafrinahamed97 <shafrinahamedjob@gmail.com>
2024-05-29 19:07:24 +0600

    Added current time zone

--- date_time_1.php
+++ date_time_1.php
@@ -1,7 +1,8 @@
 <?php
 
-$date1 = new DateTime('2025-05-04');
-$date2 = new DateTime('2025-04-04');
+$date1 = new DateTime('now', new DateTimeZone('Asia/Dhaka'));
+$date2 = new DateTime('1997-05-04');
 
 $difference = $date1->diff($date2);
-echo $difference->format('%Y Years %m months %d days') . "\n";
+echo $difference->format('%Y Years %m month(s) %d days') . "\n";
+echo $date1->format('d-m-y g:i:s a');
