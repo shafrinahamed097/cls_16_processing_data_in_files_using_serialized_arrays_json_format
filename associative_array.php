f65909d08db543f9112b59cf318d6666e5fab349
shafrinahamed97 <shafrinahamedjob@gmail.com>
2024-05-30 16:49:24 +0600

    Json To Associative array using json decode

--- associative_array.php
+++ associative_array.php
@@ -10,9 +10,26 @@ $person = array(
 
 $personString = serialize($person);
 echo $personString ."\n";
+// Output: a:3:{s:4:"fname";s:4:"John";s:5:"lname";s:3:"Doe";s:3:"age";i:25;}
 
 
-// Associative Array to Json Convertion
+// Associative Array to Json Convert
 
 $personJson = json_encode($person); 
 echo $personJson ."\n";
+// Output: {"fname":"John","lname":"Doe","age":25}
+
+// Json Decode
+
+$personDecode = json_decode($personJson,true);   
+print_r($personDecode);
+/*
+  Output:
+
+  Array
+(
+    [fname] => John
+    [lname] => Doe
+    [age] => 25
+)   
+*/
