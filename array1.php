d3d757c11b142de1f91a8d8964d7031f418ec328
shafrinahamed97 <shafrinahamedjob@gmail.com>
2024-05-30 13:44:45 +0600

    Array to Json convert

---
+++ array1.php
@@ -0,0 +1,17 @@
+<?php
+
+$fruits = array('apple', 'banana', 'orange', 'plum', 'dates', 'mango');
+$allfruits1 = implode(', ', $fruits);
+echo $allfruits1 ."\n";
+// apple, banana, orange, plum, dates, mango
+
+// serialize
+$allfruits2 = serialize($fruits);
+echo $allfruits2 ."\n";
+// a:6:{i:0;s:5:"apple";i:1;s:6:"banana";i:2;s:6:"orange";i:3;s:4:"plum";i:4;s:5:"dates";i:5;s:5:"mango";}
+
+
+// json
+$allfruits3 = json_encode($fruits);
+echo $allfruits3;
+// ["apple","banana","orange","plum","dates","mango"]
