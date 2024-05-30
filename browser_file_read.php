cf6a9412f44ac6aeaf8f994613ce401c0eb0b7b6
shafrinahamed97 <shafrinahamedjob@gmail.com>
2024-05-30 20:20:00 +0600

    Browser file read data json encoder

--- browser_file_read.php
+++ browser_file_read.php
@@ -4,6 +4,7 @@ $websitedata = file_get_contents('https://postman-echo.com/get?form=ostad');
 
 $endTime = microtime(true);
 echo $websitedata;
+print_r (json_decode($websitedata, true));
 echo "\n\n\n";
 $execution_time = $endTime - $start_time;
 echo $execution_time;
